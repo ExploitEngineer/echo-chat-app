bd706475576002ef884142052767bafaa2e8a4e0
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-18 21:40:00 +0500

    feat: add message controller to get messages between users

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import User from "../models/user.model.ts";
+import Message from "../models/message.model.ts";
 
 export const getUsersForSidebar = async (req: Request, res: Response) => {
   try {
@@ -12,6 +13,30 @@ export const getUsersForSidebar = async (req: Request, res: Response) => {
   } catch (err) {
     if (err instanceof Error) {
       console.log("Error in getUsersForSidebar: ", err.message);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+};
+
+export const getMessages = async (req: Request, res: Response) => {
+  try {
+    const {
+      params: { id: userToChatId },
+    } = req;
+
+    const myId = (req as any).user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { myId: myId, receiverId: userToChatId },
+        { myId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log("Error in getMessages controller: ", err.message);
       res.status(500).json({ error: "Internal server error" });
     }
   }
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import type { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.ts";
-import { getUsersForSidebar } from "../controllers/message.controller.ts";
+import {
+  getUsersForSidebar,
+  getMessages,
+} from "../controllers/message.controller.ts";
 
 const router: Router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
