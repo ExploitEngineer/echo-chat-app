c8c54e60d71a39c94ddcfd1279e83e6949699352
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-18 23:47:03 +0500

    feat: add send messages route

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import User from "../models/user.model.ts";
 import Message from "../models/message.model.ts";
+import cloudinary from "../lib/cloudinary.ts";
 
 export const getUsersForSidebar = async (req: Request, res: Response) => {
   try {
@@ -37,6 +38,41 @@ export const getMessages = async (req: Request, res: Response) => {
   } catch (err) {
     if (err instanceof Error) {
       console.log("Error in getMessages controller: ", err.message);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+};
+
+export const sendMessage = async (req: Request, res: Response) => {
+  try {
+    const {
+      body: { text, image },
+      params: { id: receiverId },
+    } = req;
+
+    const senderId = (req as any).user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    // todo: realtime functionality goes here => socket.io
+
+    res.status(201).json(newMessage);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log("Error in sendMessage controller: ", err.message);
       res.status(500).json({ error: "Internal server error" });
     }
   }
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -4,11 +4,14 @@ import { protectRoute } from "../middleware/auth.middleware.ts";
 import {
   getUsersForSidebar,
   getMessages,
+  sendMessage,
 } from "../controllers/message.controller.ts";
 
 const router: Router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
