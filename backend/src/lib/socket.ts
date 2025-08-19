6f481a7d45b80415361d98bb28a199f508bf9146
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-20 02:01:14 +0500

    feat: implement simple socket.io

--- backend/src/lib/socket.ts
+++ backend/src/lib/socket.ts
@@ -14,4 +14,12 @@ const io: SocketIOServer = new Server(server, {
   },
 });
 
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+  });
+});
+
 export { io, app, server };
